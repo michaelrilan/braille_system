953b623d5ec4b4e38e339fcc9f843d000785dcca
michaelrilan <micorilan1999@gmail.com>
2024-09-21 15:02:52 +0800

    added batch file to run

--- braille_console/settings.py
+++ braille_console/settings.py
@@ -18,39 +18,39 @@ hostname = socket.gethostname()
 ip_address = socket.gethostbyname(hostname)
 
 # The physical address to search for
-target_physical_address = "A0-D7-68-11-0C-88"
+# target_physical_address = "A0-D7-68-11-0C-88"
 
-def get_ip_address():
-    try:
-        # Execute the arp command
-        result = subprocess.run(['arp', '-a'], capture_output=True, text=True)
-        output = result.stdout
+# def get_ip_address():
+#     try:
+#         # Execute the arp command
+#         result = subprocess.run(['arp', '-a'], capture_output=True, text=True)
+#         output = result.stdout
 
-        lines = output.split('\n')
-        ip_address = ''
+#         lines = output.split('\n')
+#         ip_address = ''
 
-        # Search for the line that contains the target physical address
-        for line in lines:
-            if target_physical_address.lower() in line.lower():
-                parts = line.split()
-                ip_address = parts[0]  # The IP address is the first part of the line
-                break
+#         # Search for the line that contains the target physical address
+#         for line in lines:
+#             if target_physical_address.lower() in line.lower():
+#                 parts = line.split()
+#                 ip_address = parts[0]  # The IP address is the first part of the line
+#                 break
 
-        # If the IP address is not found, get the IP address of the current device
-        if not ip_address:
-            hostname = socket.gethostname()
-            ip_address = socket.gethostbyname(hostname)
+#         # If the IP address is not found, get the IP address of the current device
+#         if not ip_address:
+#             hostname = socket.gethostname()
+#             ip_address = socket.gethostbyname(hostname)
 
-        return ip_address
+#         return ip_address
 
-    except Exception as e:
-        print(f"Error executing arp command: {e}")
-        return None
+#     except Exception as e:
+#         print(f"Error executing arp command: {e}")
+#         return None
 
-# Example usage
-IP_ADDRESS = get_ip_address()
+# # Example usage
+# IP_ADDRESS = get_ip_address()
 
-print(IP_ADDRESS)
+# print(IP_ADDRESS)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,7 +132,7 @@ DATABASES = {
         'NAME':'braille_db',
         'USER' : 'root',
         'PASSWORD':'',
-        'HOST': str(IP_ADDRESS),
+        'HOST': 'localhost',
         'PORT': '3306',
         # 'OPTIONS': {
         #     'init_command': "SET sql_mode= 'STRICT_TRANS_TABLES'",
---
+++ runserver.bat
@@ -0,0 +1,3 @@
+@echo off
+start /min python manage.py runserver 7000
+exit
